39661002652dc4ef7db0296b350b5df7d3446aeb
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-05-19 22:25:06 +0530

    Add protect propety and methods: just convention

--- OOP/moreClasses.js
+++ OOP/moreClasses.js
@@ -2,28 +2,35 @@ class Account {
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+        // Protect property - just a convention to not touch it
+        this._pin = pin;
+        this._movements = [];
+
         this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${this.owner}.`)
     }
 
     // Public interface
+
+    getMovements() {
+        return this._movements;
+    }
+
     deposit(val) {
-        this.movements.push(val);
+        this._movements.push(val);
     }
 
     withdraw(val) {
         this.deposit(-val);
     }
 
-    approveLoan(val) {
+    _approveLoan(val) {
         return true;
     }
 
     requestLoan(val) {
-        if (this.approveLoan(val)) {
+        if (this._approveLoan(val)) {
             this.deposit(val);
             console.log(`Loan approved.`)
         }
@@ -37,12 +44,16 @@ console.log(acc1);
 // acc1.movements.push(25000);
 // acc1.movements.push(-14000);
 
+// After adding these methods, these should be used.
 acc1.deposit(25000);
 acc1.withdraw(14000);
+
+// After defining getMovements
+console.log(acc1.getMovements());
 
 acc1.requestLoan(10000);
 
 // This should not be accessible - so we need data encapsulation and privacy
-acc1.approveLoan(2000);
+// acc1.approveLoan(2000); // It is not a function
 
 console.log(acc1);
