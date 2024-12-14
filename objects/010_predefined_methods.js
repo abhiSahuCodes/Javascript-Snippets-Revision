fc6c9c80a23ffd038938fa2422c8ddfa76bf3d7c
abhiSahuCodes <abhishek963sahu@gmail.com>
2024-12-14 23:08:39 +0530

    Add predefined object methds in javascript

--- objects/010_predefined_methods.js
+++ objects/010_predefined_methods.js
@@ -14,7 +14,6 @@ const user = { name: "Abhishek", age: 30, profession: "Developer" };
 
 console.log(Object.keys(user)); // Output: [ 'name', 'age', 'profession' ]
 
-
 // & ----------------- Object.values() -----------------
 
 /*
@@ -25,7 +24,7 @@ console.log(Object.keys(user)); // Output: [ 'name', 'age', 'profession' ]
 
 */
 
-console.log(Object.values(user)) // Output: [ 'Abhishek', 30, 'Developer' ]
+console.log(Object.values(user)); // Output: [ 'Abhishek', 30, 'Developer' ]
 
 // & ----------------- Object.entries() -----------------
 
@@ -51,7 +50,7 @@ Output:
 */
 
 for (const [key, value] of Object.entries(user)) {
-    console.log(`${key}: ${value}`)
+  console.log(`${key}: ${value}`);
 }
 
 /*
@@ -61,7 +60,6 @@ name: Abhishek
 age: 30
 profession: Developer
 */
-
 
 // & ----------------- Object.assign() -----------------
 
@@ -73,12 +71,11 @@ profession: Developer
 
 */
 
-const source = {hobby: "Coding"};
+const source = { hobby: "Coding" };
 
 const clone = Object.assign({}, user); // Shallow copy of 'user'
 
-console.log(clone) // Output: { name: 'Abhishek', age: 30, profession: 'Developer' }
-
+console.log(clone); // Output: { name: 'Abhishek', age: 30, profession: 'Developer' }
 
 const merged = Object.assign({}, user, source); // Merging 'user' and 'source'
 console.log(merged);
@@ -88,4 +85,74 @@ Output:
 
 { name: 'Abhishek', age: 30, profession: 'Developer', hobby: 'Coding' }
 */
+
+// & ----------------- Object.freeze() -----------------
+
+/*
+^ Purpose: 
+    Freezes an object so its properties cannot be added, deleted, or modified.
+^ Use case: 
+    When you want to make an object immutable.
+
+*/
+
+const frozenObject = Object.freeze({ constant: true });
+frozenObject.constant = false; // No effect
+delete frozenObject.constant; // No effect
+
+console.log(frozenObject);
+
+const thisObject = {
+  name: "Abhishek",
+  age: 31,
+};
+
+const frozenObject2 = Object.freeze(thisObject);
+
+console.log(frozenObject2);
+
+frozenObject2.age = 23;
+
+delete frozenObject2.name;
+
+console.log(frozenObject2);
+
+// & ----------------- Object.seal() -----------------
+
+/*
+^ Purpose: 
+    Seals an object, allowing property modification but preventing addition or deletion.
+^ Use case: 
+    When you want to allow changes to values but restrict structural changes.
+
+*/
+
+const sealedObject = Object.seal({ name: "Abhishek" });
+sealedObject.name = "Sahu"; // Allowed
+sealedObject.age = 31; // Ignored
+delete sealedObject.name; // Ignored
+console.log(sealedObject); // Output: { name: "Sahu" }
+
+// & ----------------- Object.create() -----------------
+
+/*
+^ Purpose: 
+    Creates a new object with a specified prototype.
+^ Use case: 
+    To set up inheritance or create objects with specific prototypes.
+
+*/
+
+const prototype = {
+  name: "Rohan",
+  greet() {
+    return "Hello!";
+  },
+};
+
+const newObject = Object.create(prototype);
+console.log(newObject); // Output: {}
+console.log(newObject.name); // Output: "Rohan"
+console.log(newObject.greet()); // Output: "Hello!"
+
 
