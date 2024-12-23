38484fb58f6e4645c6bb127f33bd0d5d45d26d58
abhiSahuCodes <abhishek963sahu@gmail.com>
2024-12-24 00:32:01 +0530

    Add update to chaining promises in javascript

--- promise/03_chaining_promises.js
+++ promise/03_chaining_promises.js
@@ -35,7 +35,6 @@ new Promise((resolve, reject) => {
     console.error("Error:", error);
   });
 
-
 /*
 ^ How It Works:
 
@@ -44,3 +43,65 @@ The second .then() receives 2, processes it, and returns 6.
 The chain continues until all .then() methods are resolved or an error occurs.
 
 */
+
+// & -------------------- Example: Chaining Promises with API Calls --------------------
+
+// Fetching user data
+const fetchUser = () => {
+  return new Promise((resolve, reject) => {
+    fetch("https://dummyapi.online/api/users/1")
+      .then((response) => {
+        if (!response.ok) {
+          reject(`HTTP Error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((userData) => {
+        console.log("Fetched User Data:", userData);
+        resolve(userData);
+      })
+      .catch((error) => {
+        reject(`Fetch Error: ${error}`);
+      });
+  });
+};
+
+// Fetching user orders
+const fetchUserOrders = (userId) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Fetched Orders for User ID: ${userId}`);
+      resolve([
+        {
+          orderId: 1,
+          laptop: "Laptop",
+        },
+        {
+          orderId: 2,
+          laptop: "Smartphone",
+        },
+      ]);
+    }, 1500); // For 1.5-second delay
+  });
+};
+
+// Chaining promises
+
+fetchUser()
+  .then((user) => {
+    const { id, name, address } = user;
+    console.log(`User Name: ${name}`);
+    console.log(
+      `User Address: ${address.street}, ${address.city}, ${address.state} ${address.zipcode}`
+    );
+    return fetchUserOrders(id); // Fetch user's orders
+  })
+  .then((orders) => {
+    console.log("Orders:", orders);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  })
+  .finally(() => {
+    console.log("Operation complete.");
+  });
