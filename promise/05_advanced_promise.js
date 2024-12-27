0f3f46772fdd554d9ab688d089c883298e15afd6
abhiSahuCodes <abhishek963sahu@gmail.com>
2024-12-27 12:49:12 +0530

    Add advanced promise: Sequential execution with reduce

--- promise/05_advanced_promise.js
+++ promise/05_advanced_promise.js
@@ -38,3 +38,47 @@ If all are rejected, then:
 
 */
 
+// & -------------------- 2. Sequential Execution with reduce --------------------
+
+// Sometimes you need to execute promises sequentially instead of in parallel. This can be achieved using Array.reduce.
+
+const tasks = [
+    () => Promise.resolve("Task 1 completed."),
+    () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve("Task 2 completed."), 1000)
+      ),
+    () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve("Task 3 completed."), 500)
+      ),
+  ];
+  
+  tasks.reduce((prevPromise, task) => {
+      return prevPromise.then(() => task().then(console.log))
+  }, Promise.resolve())
+  .then(() => console.log("All tasks completed."))
+  
+  /*
+  Output:
+  Task 1 completed.
+  Task 2 completed.
+  Task 3 completed.
+  All tasks completed.
+  
+  */
+  
+  /*
+  Here, the tasks are fulfilled in a serial manner. The Promise.resolve() in tasks.reduce is given as the reduce function needs an intial promise.
+  This promise is not created by the user. When the reduce function starts it becomes as a prevPromise, making the first iteration starts with a 
+  resolved promise and then all given promises are fulfilled or rejected in a series.
+  
+  This use of Promise.resolve() as the first resolved promise leads to correct execution of given promises in the array. Otherwise, if task()[0]
+  is given as the first promise then it will be treated differently then other promises in the array i.e. task()[1], and task()[2].
+  
+  Though, the task()[2] is faster than task()[1], it waits for the task()[1] to complete, and then it is resolved.
+  
+  This way we can perform a series of promise execution irrespective of their time to resolve. This is useful when we want a series of API to be fetched
+  in a manner but they are fetched with variable speed and we want a slower API response to show earlier than a fast API response.
+  
+  */
